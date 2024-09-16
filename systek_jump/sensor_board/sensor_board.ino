a73b91ff0a43bfb6f921290a1707b1b75feafcc3
Aleksander Grande <aleksander.grande@mattilsynet.no>
2024-09-16 08:05:04 +0200

    Merge

--- systek_jump/sensor_board/sensor_board.ino
+++ systek_jump/sensor_board/sensor_board.ino
@@ -14,31 +14,21 @@ typedef struct struct_message {
 struct_message myData;
 
 esp_now_peer_info_t peerInfo;
-
-// callback when data is sent
-void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status) {
-  Serial.print("\r\nLast Packet Send Status:\t");
-  Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-}
+              
 String error ="";
 void setup() {
-  delay(5000);
   // Init Serial Monitor
-  Serial.begin(115200);
-  Serial.print("Serial start");
-  Wire.begin();
-  BMI160.begin(BMI160GenClass::I2C_MODE, Wire, i2c_addr);
+  //BMI160.begin(BMI160GenClass::I2C_MODE, Wire, i2c_addr);
+  BMI160.begin(BMI160GenClass::I2C_MODE,i2c_addr,select_pin);
+
 
   // Set device as a Wi-Fi Station
   WiFi.mode(WIFI_STA);
 
   // Init ESP-NOW
   if (esp_now_init() != ESP_OK) {
-    Serial.println("Error initializing ESP-NOW");
     return;
   }
-
-  esp_now_register_send_cb(OnDataSent);
 
   // Register peer
   memcpy(peerInfo.peer_addr, broadcastAddress, 6);
@@ -47,8 +37,6 @@ void setup() {
 
   // Add peer
   if (esp_now_add_peer(&peerInfo) != ESP_OK) {
-    Serial.println("Failed to add peer");
-    error = "FAILED TO ADD PEER";
     return;
   }
 }
@@ -59,45 +47,9 @@ void loop() {
   BMI160.readAccelerometer(gx, gy, gz);
   float at = sqrt(gx * gx + gy * gy + gz * gz);
   if (at > 35000) {
-    Serial.println(error);
-    Serial.println(at);
-    myData.boardNo = 2;
+    myData.boardNo = 1;
     esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    if (result == ESP_OK) {
-      Serial.println("Sent with success");
-    }
-    else {
-      Serial.println("Error sending the data");
-      
-    }
     delay(350);
   }
-  /*
-    // display tab-separated gyro x/y/z values
-    Serial.print("g:\t");
-    Serial.print(gx);
-    Serial.print("\t");
-    Serial.print(gy);
-    Serial.print("\t");
-    Serial.print(gz);
-    Serial.println();*/
-  /*
-    delay(500);
-    // Set values to send
-    myData.gx = 1;
-    myData.gy = 1;
-    myData.gz = 1;
-    myData.ax = 1;
-    myData.ay = 1;
-    myData.az = 1;
-    esp_err_t result = esp_now_send(broadcastAddress, (uint8_t *) &myData, sizeof(myData));
-
-    if (result == ESP_OK) {
-    Serial.println("Sent with success");
-    }
-    else {
-    Serial.println("Error sending the data");
-    }*/
 
 }
